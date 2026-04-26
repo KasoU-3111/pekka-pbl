8e2d366e408f8b2597d92c1fe17aa09130a4c031
KasoU3111 <shouryadhondwad1113@gmail.com>
2026-04-26 20:58:53 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -13,10 +13,12 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base : "/firstprincipleshq",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
   },
+
 }));
